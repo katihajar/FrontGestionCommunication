beb4b6175c2b437e7bb4549fdaac8fabbffad6fd
Katihajar <hajar.kati@edu.uca.ma>
2023-02-21 14:35:32 +0100

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../controller/service/auth.service";
-import {Userauth} from "../controller/model/userauth";
-import {User} from "../controller/model/user";
+import { AuthService } from "../controller/service/auth.service";
+import { Userauth } from "../controller/model/userauth";
+import { User } from "../controller/model/user";
 
 @Component({
   selector: 'app-login',
@@ -9,13 +9,15 @@ import {User} from "../controller/model/user";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
   username: string = String();
-  password: string= String();
-  errorMessage= 'Invalid Credentials';
-  successMessage: string  = String();
-  invalidLogin= false;
-  loginSuccess= false;
-  constructor(private authService: AuthService) {}
+  password: string = String();
+  errorMessage = 'Invalid Credentials';
+  successMessage: string = String();
+  invalidLogin = false;
+  loginSuccess = false;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
     this.User = new User();
@@ -35,20 +37,18 @@ export class LoginComponent implements OnInit {
   set UserAuth(value: Userauth) {
     this.authService.UserAuth = value;
   }
-  handleLogin(){
+  handleLogin() {
     this.authService.Login(this.username, this.password).subscribe((data) => {
-       // @ts-ignore
-      this.UserAuth =data.body;
-       this.User = this.UserAuth.user;
+      // @ts-ignore
+      this.UserAuth = data.body;
+      this.User = this.UserAuth.user;
       console.log('tken: ' + this.UserAuth.accessToken);
       console.log('us: ' + JSON.stringify(this.User));
       console.log('auth: ' + JSON.stringify(this.User.authorities));
       console.log('role: ' + this.User.roles[0].name);
-
       this.invalidLogin = false;
       this.loginSuccess = true;
       this.successMessage = 'Login Successful';
-      // redirect to main page
     }, () => {
       this.invalidLogin = true;
       this.loginSuccess = false;

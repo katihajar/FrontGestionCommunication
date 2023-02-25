d00a306174717c2cae9c2d9202c19ddc5b245149
kati hajar <hakati1999@hotmail.com>
2023-02-25 23:18:42 +0100

    cc

--- src/app/forbidden/forbidden.component.ts
+++ src/app/forbidden/forbidden.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forbidden',
@@ -8,23 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForbiddenComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit(): void {
     var str = document.getElementsByTagName('div')[0].innerHTML.toString();
+    console.log('hna string value : '+str);  
     var i = 0;
     document.getElementsByTagName('div')[0].innerHTML = "";
 
     setTimeout(function() {
-      var se = setInterval(function() {
-        i++;
-        document.getElementsByTagName('div')[0].innerHTML = str.slice(0, i) + "|";
-        if (i == str.length) {
-          clearInterval(se);
-          document.getElementsByTagName('div')[0].innerHTML = str;
-        }
-      }, 10);
-    },0);
+    var se = setInterval(function() {
+    i++;
+    document.getElementsByTagName('div')[0].innerHTML = str.slice(0, i) + "|";
+  if (i == str.length) {
+      clearInterval(se);
+     document.getElementsByTagName('div')[0].innerHTML = str;
+  }else{
+    document.getElementsByTagName('div')[0].innerHTML = str.slice(0, i) + "|";
   }
+}, 10);
+},0);
+  }
+  
 
 }
